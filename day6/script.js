56fc0b1f6b6db3ac417568af97accc77b3edee23
phadlluk <phadlluk@gmail.com>
2022-08-23 23:48:59 +0100

    feat:30 days of javascript setup

---
+++ day6/script.js
@@ -0,0 +1,20 @@
+//1.Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+//for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+//while loop
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+//do while loop
+let a = 0;
+do {
+  console.log(a);
+  a++;
+} while (a <= 10);
+
+//2.Iterate 10 to 0 using for loop, do the same using while and do while loop
