385b7c6e390116b6c898b9018a82f2e9ec7d66ab
phadlluk <phadlluk@gmail.com>
2022-08-25 00:29:18 +0100

    feat:30 days of javascript setup

--- day6/script.js
+++ day6/script.js
@@ -5,11 +5,11 @@ for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
 //while loop
-let i = 0;
-while (i <= 10) {
-  console.log(i);
-  i++;
-}
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
 //do while loop
 let a = 0;
 do {
@@ -18,3 +18,22 @@ do {
 } while (a <= 10);
 
 //2.Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+//for loop
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+
+//while loop
+let i = 10;
+while (i >= 0) {
+  console.log(i);
+  i--;
+}
+
+//do while loop
+let b = 10;
+do {
+  console.log(b);
+  b--;
+} while (b >= 0);
